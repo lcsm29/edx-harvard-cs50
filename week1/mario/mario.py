ef72bd21df33689d44bb82bcbcd15d068cfbd6d7
lcsm29 <lcsm29@outlook.com>
2021-08-24 04:41:23 +0900

    Delete mario.py from week1

--- week1/mario/mario.py
+++
@@ -1,5 +0,0 @@
-height = 0
-while(not 1 <= height <= 8):
-    height = int(input("Height: "))
-for line in [' ' * (height-i) + '#' * i for i in range(1, height + 1)]:
-    print(line + '  ' + line[::-1])
